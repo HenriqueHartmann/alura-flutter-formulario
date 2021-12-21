13a357001e14c1bca340d533ab65e8cd5d25e8f0
HenriqueHartmann <alemaohartmann@gmail.com>
2021-12-21 10:43:20 -0300

    adjust keyboard in quantity and value

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cadastrando produto'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
+          appBar: AppBar(
+            title: const Text('Cadastrando produto'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(children: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(top: 16.0),
                 child: TextField(
                     controller: _controladorNome,
-                decoration: const InputDecoration()
+                    decoration: const InputDecoration()),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _controladorQuantidade,
+                  decoration: const InputDecoration(),
+                  keyboardType: TextInputType.number,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 16.0),
                 child: TextField(
-                    controller: _controladorQuantidade,
-                    decoration: const InputDecoration(),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: TextField(
-                    controller: _controladorValor,
-                    decoration: const InputDecoration(),
+                  controller: _controladorValor,
+                  decoration: const InputDecoration(),
+                  keyboardType: TextInputType.number,
                 ),
               ),
               Padding(
@@ -49,21 +49,20 @@ class MyApp extends StatelessWidget {
                     child: const Text('Cadastrar'),
                     onPressed: () {
                       final String nome = _controladorNome.text;
-                      final int? quantidade = int.tryParse(_controladorQuantidade.text);
-                      final double? valor = double.tryParse(_controladorValor.text);
+                      final int? quantidade =
+                          int.tryParse(_controladorQuantidade.text);
+                      final double? valor =
+                          double.tryParse(_controladorValor.text);
 
                       if (quantidade != null && valor != null) {
-                        final Produto produtoNovo = Produto(
-                            nome, quantidade, valor);
+                        final Produto produtoNovo =
+                            Produto(nome, quantidade, valor);
                         print(produtoNovo);
                       }
-                    }
-                ),
+                    }),
               )
-            ]
-          ),
-        )
-      ),
+            ]),
+          )),
     );
   }
 }
